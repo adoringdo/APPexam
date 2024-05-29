a674e654b8ce9a7e5e957bdeb6322baaf3c73275
adoringdo <adenisovaite@gmail.com>
2024-05-29 07:40:46 +0300

    log in func and random pic

--- app/src/main/java/com/example/examappadriana/Login.java
+++ app/src/main/java/com/example/examappadriana/Login.java
@@ -1,17 +1,28 @@
 package com.example.examappadriana;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        TextInputEditText usernameText, passwordText;
+        Button buttonLogin;
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
@@ -20,5 +31,28 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        usernameText = findViewById(R.id.username);
+        passwordText = findViewById(R.id.password);
+        buttonLogin = findViewById(R.id.loginBtn);
+        TextView errorMessage = findViewById(R.id.loginError);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Retrieve entered username and password
+                String username = usernameText.getText().toString();
+                String password = passwordText.getText().toString();
+
+                // Implement authentication logic here
+                if (username.equals("Admin") && password.equals("12345")) {
+                    startActivity(new Intent(Login.this, MainActivity.class));
+                } else {
+                    errorMessage.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/examappadriana/TextWindow.java
+++ app/src/main/java/com/example/examappadriana/TextWindow.java
@@ -1,7 +1,6 @@
 package com.example.examappadriana;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.lang.reflect.Array;
-import java.util.Random;
-
-public class MainActivity extends AppCompatActivity {
-
-
-
-public String[] randomPic = {"dog", "cat", "panda"};
+public class TextWindow extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_text_window);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        ImageView myImageView = (ImageView) findViewById(R.id.greetingPic);
-        final int randomNr = new Random().nextInt(3); // [0, 60] + 20 => [20, 80]
-        myImageView.setImageResource(getResources().getIdentifier(randomPic[randomNr], "drawable", getPackageName()));
-
     }
 }
